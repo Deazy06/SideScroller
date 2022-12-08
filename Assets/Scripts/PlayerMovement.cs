8a015d60bf8295b2c4ccf580053c26f3ca8829e0
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2022-12-08 18:41:25 +0100

    Game code/etc

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Movementspeed = 5;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * Movementspeed;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector3(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
