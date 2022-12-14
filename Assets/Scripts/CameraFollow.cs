92dfe163f6f9522cad0864938edfb5cfce17d779
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2022-12-14 10:44:32 +0100

    Filip improvement movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private Vector3 offset = new Vector3(0f, 0f, -10f);
-    private float smoothTime = 0.45f;
+    public float smoothTime = 0.45f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform target;
