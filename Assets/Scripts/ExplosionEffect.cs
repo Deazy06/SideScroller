d278bb8d3b49b40c68f38a975e5e8944b39ea58b
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2022-12-14 09:56:02 +0100

    Blood particle done xD :skull:

--- Assets/Scripts/ExplosionEffect.cs
+++ Assets/Scripts/ExplosionEffect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExplosionEffect : MonoBehaviour
 {
-    
+    public ParticleSystem particleEffect;
     void Start()
     {
        
@@ -14,6 +14,7 @@ public class ExplosionEffect : MonoBehaviour
     
     public void EnemyDie(humanSuffering enemy)
     {
-        
+        particleEffect.transform.position = enemy.transform.position;
+        particleEffect.Play();
     }
 }
